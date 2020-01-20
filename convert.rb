5830fe2040285a0830e278fa917f054d9a96377b
saugatkhadka <ksaugat@gmail.com>
2020-01-20 13:03:08 +0545

    Initiated the solution for converting a given number to words
    The project is intended to create a simple program to generate a
    list of possible combinations of words for a given set of numbers
    from a given dictionary of words.
    
    The program will be written with a bruteforce approach at first and
    progressively be refined in each iterations until a viable solution
    can be found, which can generate the combinations within 1000ms.

---
+++ convert.rb
@@ -0,0 +1,8 @@
+# Convert numbers to word
+
+# Steps
+# 1. Take a number as input. Expose a public function
+# 2. Separate the last 10 numbers, or those with dashes
+# 3. Generate the possible combinations from the number given
+# 4. Match the word combinations with the library/dictionary
+# 5. Create an array of the matching combinations of words and return them in an array
